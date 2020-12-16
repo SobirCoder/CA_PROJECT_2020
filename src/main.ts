9f7d0eb299110b5cedd30101c64cde4faaf4248a
Sadullaev Sobirjon <ssadullaev1999@gmail.com>
2020-12-16 23:26:41 +0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,88 @@
-import { redBlackTree } from './types/trees.js';
-import { Node } from './types/elems.js';
+import { vertex, adjacenNode } from './types/graph.js';
 
-let tr = new redBlackTree<number>();
+let vxs :vertex<string>[] = [];
 
 
-tr.insert(Node.create(65));
-tr.insert(Node.create(45));
-tr.insert(Node.create(66));
-tr.insert(Node.create(86));
-tr.insert(Node.create(87));
-tr.insert(Node.create(35));
-tr.insert(Node.create(25));
-tr.insert(Node.create(15));
 
-tr.insert(Node.create(1));
-tr.insert(Node.create(2));
-tr.insert(Node.create(3));
-tr.insert(Node.create(39));
-tr.insert(Node.create(38));
-tr.insert(Node.create(27));
-tr.insert(Node.create(78));
+vxs.push(vertex.create('a', [adjacenNode.create(4, 'b'), adjacenNode.create(4, 'h')]));
+vxs.push(vertex.create('b', [adjacenNode.create(4, 'a'), adjacenNode.create(11, 'h'),
+                              adjacenNode.create(8, 'c')]));
+vxs.push(vertex.create('h', [adjacenNode.create(8, 'a'), adjacenNode.create(11, 'b'),
+                              adjacenNode.create(7, 'i'), adjacenNode.create(1, 'g')]));
+vxs.push(vertex.create('c', [adjacenNode.create(8, 'b'), adjacenNode.create(2, 'i'),
+                              adjacenNode.create(4, 'f'), adjacenNode.create(7, 'd')]));
+vxs.push(vertex.create('i', [adjacenNode.create(7, 'h'), adjacenNode.create(6, 'g'),
+                              adjacenNode.create(2, 'c')]));
+vxs.push(vertex.create('g', [adjacenNode.create(1, 'h'), adjacenNode.create(6, 'i'),
+                              adjacenNode.create(2, 'f')]));
+vxs.push(vertex.create('d', [adjacenNode.create(7, 'c'), adjacenNode.create(14, 'f'),
+                              adjacenNode.create(9, 'e')]));
+vxs.push(vertex.create('f', [adjacenNode.create(4, 'c'), adjacenNode.create(14, 'd'),
+                              adjacenNode.create(2, 'g'), adjacenNode.create(10, 'e')]));
+vxs.push(vertex.create('e', [adjacenNode.create(9, 'd'), adjacenNode.create(10, 'e')]));
 
-console.log(tr.get());
 
-tr.delete(78);
 
-console.log(tr.get());
 
-tr.delete(25);
 
-console.log(tr.get());
+
+
+
+
+
+
+// vxs.push(vertex.create(1, [2, 0]));
+// vxs.push(vertex.create(0, [1, 2, 2]));
+// vxs.push(vertex.create(2, [0, 1, 3]));
+// vxs.push(vertex.create(3, [3, 2]));
+
+// dfs(vxs[2], vxs, undefined);
+
+// vxs.push(vertex.create(0, [1, 2]));
+// vxs.push(vertex.create(1, [0, 2, 3]));
+// vxs.push(vertex.create(2, [0, 1, 4, 7]));
+// vxs.push(vertex.create(3, [1, 4]));
+// vxs.push(vertex.create(4, [2, 3, 5, 7]));
+// vxs.push(vertex.create(5, [4, 6]));
+// vxs.push(vertex.create(6, [5]));
+// vxs.push(vertex.create(7, [2, 4]));
+
+//dfs(vxs[0], vxs, undefined);
+// bfs(vxs);
+
+
+// import { redBlackTree } from './types/trees.js';
+// import { Node } from './types/elems.js';
+
+// let tr = new redBlackTree<number>();
+
+
+// tr.insert(Node.create(65));
+// tr.insert(Node.create(45));
+// tr.insert(Node.create(66));
+// tr.insert(Node.create(86));
+// tr.insert(Node.create(87));
+// tr.insert(Node.create(35));
+// tr.insert(Node.create(25));
+// tr.insert(Node.create(15));
+
+// tr.insert(Node.create(1));
+// tr.insert(Node.create(2));
+// tr.insert(Node.create(3));
+// tr.insert(Node.create(39));
+// tr.insert(Node.create(38));
+// tr.insert(Node.create(27));
+// tr.insert(Node.create(78));
+
+// console.log(tr.get());
+
+// tr.delete(78);
+
+// console.log(tr.get());
+
+// tr.delete(25);
+
+// console.log(tr.get());
 
 
 // console.log('root: ' + root!.value);
--- src/types/graph.ts
+++ src/types/graph.ts
@@ -1,7 +1,75 @@
-type aj_list = [number, number, number];
+class adjacenNode<TValue>
+{
+  constructor(public weight :number, public value :TValue) {}
 
-function dfs(vertices :aj_list[], visited :boolean[]) {
-  if (!visited) visited = new Array<boolean>(vertices.length);
-
-
+  public static create<T>(weight :number, value :T)
+  {
+    return new adjacenNode(weight, value);
+  }
 }
+
+class vertex<TValue>
+{
+  constructor(public value :TValue, public adjacentNodes :adjacenNode<TValue>[]) {}
+
+  addAdjacentNode(weight :number, value :TValue)
+  {
+    this.adjacentNodes.push(adjacenNode.create(weight, value));
+  }
+
+  static create<T>(val :T, adjacentNodes :adjacenNode<T>[])
+  {
+    return new vertex(val, adjacentNodes || []);
+  }
+}
+
+// function dfs<T>(vc :vertex<T>, vertices :vertex<T>[], visited? :boolean[]) {
+//   if (!visited) visited = new Array<boolean>(vertices.length);
+
+//   if (visited[Number(vc.value)]) return;
+//   visited[Number(vc.value)] = true;
+
+//   console.log(vc.value + ' ');
+
+//   for (let v of vc.adjacentNodes)
+//     dfs(vertices.find(t => t.value == v)!, vertices, visited);
+// }
+
+// function bfs<T>(vertices :vertex<T>[]) {
+//   let visited :T[] = [],
+//       childs :vertex<T>[] = [vertices[0]],
+//       temp_childs :vertex<T>[],
+//       vc :vertex<T>;
+
+//   while (childs.length)
+//   {
+//     temp_childs = [];
+//     visited.splice(visited.length, 0, ...childs.map(chd => chd.value));
+//     for (let chd of childs)
+//     {
+//       console.log(chd.value);
+//       temp_childs.splice(temp_childs.length, 0,
+//           ...chd.adjacentNodes.filter(nd => !visited.some(v => v == nd))
+//                               .map(nd => vertices.find(v => v.value == nd)!));
+//     }
+
+//     childs = temp_childs;
+//   }
+// }
+
+function mst_prim<T>(vertices :vertex<T>[])
+{
+  let mst :vertex<T>[] = [],
+    possibleEdges :[T, T, number][];
+
+  while (mst.length != vertices.length)
+  {
+    let node = mst[mst.length - 1] || vertices[0];
+    let rnd = vertices.find(vx => vx.value == node.value)!;
+
+    let nextNode = rnd.adjacentNodes.filter(vx => !mst.some(v => v.value == vx.value))
+                      .push();
+  }
+}
+
+export { vertex, adjacenNode };
